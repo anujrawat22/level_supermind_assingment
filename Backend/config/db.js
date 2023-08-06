e9dacf6e490e1470035f960b1ab0f02fde550f4b
anujrawat22 <anuj22rawat20@gmail.com>
2023-08-06 20:29:44 +0530

    authentication middlware

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,7 +5,7 @@ const password = process.env.password
 
 const sequelize = new Sequelize('superminddb', username, password, {
   host: 'database-1.cml5tjlvnix4.ap-south-1.rds.amazonaws.com',
-  dialect: 'mysql'/* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+  dialect: 'mysql'
 });
 
 
